dcc83939d7f7e6ef6100614039b88dd45d87d626
dwyfrequency <jdwy215@gmail.com>
2018-08-05 21:46:45 -0400

    adding map for counter with key val

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom";
 import Counter from "./counter";
 
 class counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
   render() {
-    return (
-      <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
-      </div>
-    );
+    return <div>{this.state.counters.map(i => <Counter key={i.id} />)}</div>;
   }
 }
 

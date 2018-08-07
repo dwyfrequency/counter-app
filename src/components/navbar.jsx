4e58f36028cdf9b509904f2712face78325bc375
dwyfrequency <jdwy215@gmail.com>
2018-08-07 07:00:39 -0400

    adding destructuring to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
-const NavBar = props => {
+// destructing the props passed in to just get the total count - must wrap in parans ()
+const NavBar = ({ totalCount }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar
         <span className="badge badge-pill badge-secondary m-2">
-          {props.totalCount}
+          {totalCount}
         </span>
       </a>
     </nav>
